c8a79cbf222683c085a2e1668e66b37b89289899
gitstar-oc <chandankarom07@gmail.com>
2024-09-10 17:37:56 +0530

    Done some changes, checking if the issue is still there

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -7,12 +7,12 @@ const Logo = () => {
     <Image
       src="/TransparentLogo.png"
       width={40}
-      height={0}
+      height={30}
       alt="Picture of the author"
       className='group-hover:opacity-100  opacity-80'
     /> 
     <span className='relative'>
-    <p className={`${economica.className}  mt-2 ml-1 text-transparent bg-clip-text bg-[linear-gradient(180deg,_#25AFAA_0%,_#4C054C_100%)] opacity-80 group-hover:opacity-100`} style={{fontSize : "20px"}} >LearnAI </p>
+    <p className={`${economica.className} text-[20px]  mt-2 ml-1 text-transparent bg-clip-text bg-[linear-gradient(180deg,_#25AFAA_0%,_#4C054C_100%)] opacity-80 group-hover:opacity-100`} >LearnAI </p>
     <span className="underlineEffect"></span>
     </span>
     </span>
