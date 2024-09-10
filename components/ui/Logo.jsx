3aa1d0846326184e61a79aa8e75446782f7c4314
gitstar-oc <chandankarom07@gmail.com>
2024-09-10 08:40:38 +0530

    Updated Logo

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -1,13 +1,21 @@
 import Image from 'next/image'
+import { economica } from "@/lib/fonts"
 
 const Logo = () => {
   return (
+    <span className='flex group'>
     <Image
-      src="/MainLogo.png"
-      width={100}
-      height={100}
+      src="/Logo.png"
+      width={46}
+      height={28}
       alt="Picture of the author"
-    />
+      className='group-hover:opacity-100  opacity-80'
+    /> 
+    <span className='relative'>
+    <p className={`${economica.className} text-[26px] mt-2 ml-1 text-transparent bg-clip-text bg-[linear-gradient(180deg,_#25AFAA_0%,_#4C054C_100%)] opacity-80 group-hover:opacity-100`} >LearnAI </p>
+    <span className="underlineEffect"></span>
+    </span>
+    </span>
   )
 }
 
