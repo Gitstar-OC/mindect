e96a590f4d59f5e98e91e3f6dd41906aff8fd3da
gitstar-oc <chandankarom07@gmail.com>
2024-09-10 23:49:48 +0530

    Added more things to seo, will be adding more by tomorrow

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,19 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <head>
-      <link rel="icon" href="/Logo.png" sizes="128x128" />
-        <link rel="icon" href="/Logo.png" type="image/png" sizes="128x128" />
-        <link
-          rel="apple-touch-icon"
-          href="/Logo.png"
-          type="image/png"
-          sizes="128x128"
-        />
+        <link rel="icon" href="/Logo.png" sizes="128x128" />
+        <link rel="apple-touch-icon" href="/Logo.png" sizes="128x128" />
         <title>LearnAI</title>
+        <meta name="description" content="Learn Concepts in AI, Machine Learning, Deep Learning, Large Language Models, Speech Recognition etc. At Anytime, From Anywhere." />
+        <meta name="keywords" content="AI, Machine Learning, Deep Learning, Large Language Models, Deep Learning, Supervised Learning, Unsupervised Learning, Gradient Descent, Clustering, Anomaly Detection, Reinforcement Learning, Back Propagation , Multiclass Classification, Activation Function, Vectorization, Bias and Variance, Development Processes
+Decision Trees, Regression Model, Cost Function" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="LearnAI " />
+        <meta property="og:description" content="- Learn Concepts in AI, Machine Learning, Deep Learning, Large Language Models, Speech Recognition etc. At Anytime, From Anywhere." />
+        <meta property="og:image" content="/Logo.png" />
+        <meta property="og:url" content="https://learnai.theme-verse.com/" />
+        <meta property="og:type" content="website" />
         <meta name="google-site-verification" content="JoujTmv5oh0bgEPDWe-FC_D1gsDCmoxox9a25fkwo-M" />
       </head>
       <body>
