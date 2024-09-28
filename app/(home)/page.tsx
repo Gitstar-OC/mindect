d084165b75002b962e846f2cd0d3c7460a8aa3d7
gitstar-oc <chandankarom07@gmail.com>
2024-09-28 21:54:14 +0530

    Removed the scroll to bottom from the file

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,26 +3,8 @@ import { useState, useEffect } from "react";
 import Home from "@/components/ui/Home/HomePage";
 
 export default function HomePage() {
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setLoading(false); 
-    }, 4000); 
-
-    return () => clearTimeout(timeout); 
-  }, []);
-
-  useEffect(() => {
-    if (!loading) {
-      window.scrollTo({
-        top: document.documentElement.scrollHeight, 
-        behavior: 'smooth', 
-      });
-    }
-  }, [loading]); 
-
-  return (
+return (
     <>
       <Home />
     </>
