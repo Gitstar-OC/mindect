4d8b97959ada4bf612ea20d9088ed9d793a52db3
gitstar-oc <chandankarom07@gmail.com>
2024-09-29 23:52:17 +0530

    Added homepage for light theme

--- components/ui/Home/Button.jsx
+++ components/ui/Home/Button.jsx
@@ -5,9 +5,9 @@ import "./homepage.css";
 const Button = () => {
   return (
     <div className="text-center lg:text-start mt-6">
-      <button className="text-[20px] sm:text-[24px] md:text-[30px] rounded-[40px] border-[2px] border-[#4C054C] button p-2 pl-6 pr-4">
+      <button className="text-[20px] sm:text-[24px] md:text-[30px] bg-neutral-100 dark:bg-zinc-900 rounded-[40px] border-[2px] border-[#4C054C] button hover:bg-white dark:hover:bg-black p-2 pl-6 pr-4">
         <span
-          className={`${kiteOne.className} text-[20px] sm:text-[22px] md:text-[24px] relative`}
+          className={`${kiteOne.className} text-[20px] sm:text-[22px] md:text-[24px] relative text-black dark:text-white `}
         >
           Get Started
         </span>
