14532f640d214334608a2618029f47526773a1c7
gitstar-oc <chandankarom07@gmail.com>
2024-09-30 20:33:47 +0530

    Added icons for root toggler and added it again because it was removed

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,10 +3,40 @@ import type { ReactNode } from 'react';
 import { baseOptions } from '../layout.config';
 import { source } from '@/app/source';
 import React from 'react';
+import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle'
+
+import {USLImage, SLImage, AlgsImage} from "@/components/ui/icons"
+
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout tree={source.pageTree} {...baseOptions}   
+    sidebar={{
+      banner: (
+        <RootToggle
+          options={[
+            {
+              title: 'Algs',
+              description: 'Learning Algorithms',
+              url: '/docs/algs',
+              icon: <AlgsImage />
+            },
+            {
+              title: 'SL',
+              description: 'Supervised Learning',
+              url: '/docs/sl',
+              icon: <SLImage />
+            },
+            {
+              title: 'USL',
+              description: 'Unsupervised Learning',
+              url: '/docs/usl',
+              icon: <USLImage />
+            },
+          ]}
+        />
+      ),
+    }}>
       {children}
     </DocsLayout>
   );
