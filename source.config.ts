7697eea5ca1c25b664ed7b13178e71fe4ba1aa6e
gitstar-oc <chandankarom07@gmail.com>
2024-10-12 10:14:05 +0530

    Added 2 notebooks in the algebra section and added styling for callouts

--- source.config.ts
+++ source.config.ts
@@ -1,8 +1,6 @@
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
-import { rehypeCode } from 'fumadocs-core/mdx-plugins';
-
 
 export const { docs, meta } = defineDocs(
   {
@@ -18,6 +16,6 @@ export const { docs, meta } = defineDocs(
 export default defineConfig({
   mdxOptions: {
       remarkPlugins: [remarkMath],
-      rehypePlugins: (v) => [rehypeKatex, rehypeCode, ...v],
+      rehypePlugins: (v) => [rehypeKatex, ...v],
   },
 });
