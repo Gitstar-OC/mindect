85a62f624e211d4a25a36df4cd1c133238ae9a45
OC <chandankarom07@gmail.com>
2024-11-01 17:10:41 +0530

    Updated the /learn page

--- components/ui/Learn/MachineLearning/UnsupervisedLearningCard.jsx
+++ components/ui/Learn/MachineLearning/UnsupervisedLearningCard.jsx
@@ -25,15 +25,6 @@ const UnsupervisedLearningCard = () => {
       extraSteps={extraSteps}
       statusStyle="bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300"
       subheading="Here's what you will learn:"
-      buttonText={
-        <>
-        <div className="absolute inset-0 bg-gradient-to-r from-[#25AFAA] to-[#4C054C] rounded-lg" />
-              <div className="flex items-center px-8 py-2 rounded-[6px] relative group transition duration-200 hover:text-white bg-white dark:bg-black dark:text-white text-black dark:hover:bg-transparent  hover:bg-transparent">
-                Start Learning
-                <ChevronRightIcon className="ml-2 transition-transform duration-300 group-hover:translate-x-1" />
-              </div>
-        </>
-      }
       buttonPath="usl"
     />
   );
--- components/ui/Learn/Mathematics/LinearAlgebraCard.jsx
+++ components/ui/Learn/Mathematics/LinearAlgebraCard.jsx
@@ -33,17 +33,16 @@ const LinearAlgebraCard = () => {
       extraSteps={extraSteps}
       subheading="Here's what you will learn:"
       statusStyle=""
-      buttonText={
-        <>
-        <div className="absolute inset-0 bg-gradient-to-r from-[#25AFAA] to-[#4C054C] rounded-lg" />
-              <div className="flex items-center px-8 py-2 rounded-[6px] relative group transition duration-200 hover:text-white bg-white dark:bg-black dark:text-white text-black dark:hover:bg-transparent  hover:bg-transparent">
-                Start Learning
-                <ChevronRightIcon className="ml-2 transition-transform duration-300 group-hover:translate-x-1" />
-              </div>
-        </>
-      }
+      // buttonText={
+      //   <>
+      //   <div className="absolute inset-0 bg-gradient-to-r from-[#25AFAA] to-[#4C054C] rounded-lg" />
+      //         <div className="flex items-center px-8 py-2 rounded-[6px] relative group transition duration-200 hover:text-white bg-white dark:bg-black dark:text-white text-black dark:hover:bg-transparent  hover:bg-transparent">
+      //           Start Learning
+      //           <ChevronRightIcon className="ml-2 transition-transform duration-300 group-hover:translate-x-1" />
+      //         </div>
+      //   </>
+      // }
       buttonPath="algebra"
-      buttonStyle="cursor-not-allowed"
       isDisabled={true}
     />
   );
