c761dc09bcaa97e7d714a8efbebb9bd85e7b920f
OC <chandankarom07@gmail.com>
2024-12-05 15:28:21 +0530

    Updated the layout

--- app/learn/layout.tsx
+++ app/learn/layout.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { DocsLayout } from "fumadocs-ui/layout";
 import type { ReactNode } from "react";
 import { baseOptions } from "../layout.config";
@@ -16,65 +15,64 @@ import {
   CalculusIcon,
   ProbabilityIcon,
 } from "@/components/ui/icons";
-import { useMediaQuery } from '@/hooks/useMediaQuery';
 
 export default function Layout({ children }: { children: ReactNode }) {
-
-  const isLargerThanMd = useMediaQuery('(min-width: 768px)');
-
-  const sidebarContent = (
-    <RootToggle
-      options={[
-        {
-          title: "Algs",
-          description: "Learning Algorithms",
-          url: "/learn/algs",
-          icon: <AlgsIcon className="m-0" />,
-        },
-        {
-          title: "SL",
-          description: "Supervised Learning",
-          url: "/learn/sl",
-          icon: <SLIcon className="m-0" />,
-        },
-        {
-          title: "USL", 
-          description: "Unsupervised Learning",
-          url: "/learn/usl",
-          icon: <USLIcon className="m-0" />,
-        },
-        {
-          title: "Algebra",
-          description: "Linear Algebra", 
-          url: "/learn/algebra",
-          icon: <AlgebraIcon className="m-0" />,
-        },
-        {
-          title: "P & S",
-          description: "Probability & Statistics",
-          url: "/learn/probability",
-          icon: <ProbabilityIcon className="m-0" />,
-        },
-        {
-          title: "Calculus",
-          description: "Calculus",
-          url: "/learn/calculus",
-          icon: <CalculusIcon className="m-0" />,
-        },
-      ]}
-    />
-  );
-
   return (
     <>
-    <DocsLayout
-      tree={source.pageTree}
-      {...baseOptions}
-      sidebar={{
-        banner: !isLargerThanMd ? sidebarContent : null,
-      }}
-    >
-         <span
+      <DocsLayout
+        tree={source.pageTree}
+        {...baseOptions}
+        sidebar={{
+          banner: (
+            <div className="md:hidden flex">
+            <RootToggle
+              options={[
+                {
+                  title: "Algs",
+                  description: "Learning Algorithms",
+                  url: "/learn/algs",
+                  icon: <AlgsIcon className="m-0" />,
+                },
+                {
+                  title: "SL",
+                  description: "Supervised Learning",
+                  url: "/learn/sl",
+                  icon: <SLIcon className="m-0" />,
+                },
+                {
+                  title: "USL",
+                  description: "Unsupervised Learning",
+                  url: "/learn/usl",
+                  icon: <USLIcon className="m-0" />,
+                },
+                {
+                  title: "Algebra",
+                  description: "Linear Algebra",
+                  url: "/learn/algebra",
+                  icon: <AlgebraIcon className="m-0" />,
+                },
+                {
+                  title: "P & S",
+                  description: "Probability & Statistics",
+                  url: "/learn/probability",
+                  icon: <ProbabilityIcon className="m-0" />,
+                },
+                {
+                  title: "Calculus",
+                  description: "Calculus",
+                  url: "/learn/calculus",
+                  icon: <CalculusIcon className="m-0" />,
+                },
+              ]}
+            />
+            </div>
+          ),
+          // enabled: false,
+          // defaultOpenLevel: 3,
+          // collapsible: false
+        }}
+      >
+        <span
           className="absolute inset-0 z-[-1] h-[64rem] max-h-screen overflow-hidden"
           style={{
             backgroundImage:
@@ -106,88 +104,3 @@ export default function Layout({ children }: { children: ReactNode }) {
   );
 }
 
-
-//   return (
-//     <>
-//       <DocsLayout
-//         tree={source.pageTree}
-//         {...baseOptions}
-//         sidebar={{
-//           banner: (
-//             <RootToggle
-//               options={[
-//                 {
-//                   title: "Algs",
-//                   description: "Learning Algorithms",
-//                   url: "/learn/algs",
-//                   icon: <AlgsIcon className="m-0" />,
-//                 },
-//                 {
-//                   title: "SL",
-//                   description: "Supervised Learning",
-//                   url: "/learn/sl",
-//                   icon: <SLIcon className="m-0" />,
-//                 },
-//                 {
-//                   title: "USL",
-//                   description: "Unsupervised Learning",
-//                   url: "/learn/usl",
-//                   icon: <USLIcon className="m-0" />,
-//                 },
-//                 {
-//                   title: "Algebra",
-//                   description: "Linear Algebra",
-//                   url: "/learn/algebra",
-//                   icon: <AlgebraIcon className="m-0" />,
-//                 },
-//                 {
-//                   title: "P & S",
-//                   description: "Probability & Statistics",
-//                   url: "/learn/probability",
-//                   icon: <ProbabilityIcon className="m-0" />,
-//                 },
-//                 {
-//                   title: "Calculus",
-//                   description: "Calculus",
-//                   url: "/learn/calculus",
-//                   icon: <CalculusIcon className="m-0" />,
-//                 },
-//               ]}
-//             />
-//           ),
-//           // enabled: false,
-//           // defaultOpenLevel: 3,
-//           // collapsible: false
-//         }}
-//       >
-//         <span
-//           className="absolute inset-0 z-[-1] h-[64rem] max-h-screen overflow-hidden"
-//           style={{
-//             backgroundImage:
-//               "radial-gradient(49.63% 57.02% at 58.99% -7.2%, hsl(var(--primary)/0.1) 39.4%, transparent 100%)",
-//           }}
-//         >
-//           <GridPattern
-//             squares={[
-//               [16, 17],
-//               [18, 20],
-//               [21, 18],
-//               [15, 22],
-//               [14, 19],
-//               [20, 22],
-//               [12, 17],
-//               [19, 16],
-//               [12, 23],
-//             ]}
-//             className={cn(
-//               "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
-//               "inset-x-[10%] inset-y-[-80%] h-[200%] skew-y-12"
-//             )}
-//           />
-//         </span>
-//         {children}
-//       </DocsLayout>
-//       <Footer />
-//     </>
-//   );
-// }
