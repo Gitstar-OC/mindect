3616cffa496ae79923b92080def76ad350629f63
OC <chandankarom07@gmail.com>
2024-12-05 14:48:05 +0530

    Updated navbar and changed it properties for the learning page

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -47,6 +47,8 @@ const ListItem: React.FC<ListItemProps> = ({
     return null;
   }
 
+
+
   return (
     <li>
       <NavigationMenuLink asChild>
@@ -92,17 +94,16 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav
-      className={`${
-        isLearnPath
-          ? "relative lg:px-16 sm:px-4"
-          : `sticky top-0 lg:px-16 sm:px-4 transition-all ${
-              isScrolled
-                ? "bg-neutral-50/50 dark:bg-neutral-950/20 backdrop-blur-xl border-b border-b-slate-300 dark:border-b-slate-700/40"
-                : "bg-transparent"
-            }`
-      } p-4 h-14 flex items-center justify-between`}
-    >
+<nav
+  className={`${
+    isLearnPath 
+      ? 'relative lg:px-16 sm:px-4'
+      : `sticky top-0 lg:px-16 sm:px-4 transition-all ${
+          isScrolled
+            ? "bg-neutral-50/50 dark:bg-neutral-950/20 backdrop-blur-xl border-b border-b-slate-300 dark:border-b-slate-700/40"
+            : "bg-transparent"
+        }`
+  } p-4 h-14 flex items-center justify-between z-50`} >
       <div className="flex items-center space-x-4">
         <Link href="/" className="flex items-center space-x-2">
           <Logo />
